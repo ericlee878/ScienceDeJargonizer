315c7f3caee42ae761793c97719eb45a11d690b9
ericlee878 <eric8lee78@gmail.com>
2024-04-14 23:53:13 -0500

    modified-version

--- src/arXivscraper.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import pandas as pd
-import arxivscraper
-import arxiv
-import time
-
-## main
--- src/collectdata.py
+++ src/collectdata.py
@@ -9,11 +9,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-## array that holds all the abstracts of the articles
-id_to_texts_map = {}
-
-
-
 ## link to arxiv/cs
 arXiv_url = "https://arxiv.org/list/cs.AI/recent"
 
@@ -23,13 +18,51 @@ response = requests.get(arXiv_url)
 # Parse the HTML content
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# Finds all the divs with the metadata
+meta_divs = soup.find_all('div', class_='meta')
+
+# Dict that holds all the metadata of all the papers
+metadata_list = {}
+
+# Id of papers in database
+id = 0
+
+for meta in meta_divs:
+    # print(meta)
+    # print("--------")
+    # Parse the HTML content
+    if meta is not None:
+        metasoup = BeautifulSoup(meta, 'html.parser')
+        # Extracting the title
+        title = metasoup.find('div', class_='list-title').get_text(strip=True).replace('Title:', '').strip()
+        # Extracting the authors
+        authors = [a.get_text(strip=True) for a in metasoup.find('div', class_='list-authors').find_all('a')]
+        # Extracting the comments
+        comments = metasoup.find('div', class_='list-comments').get_text(strip=True).replace('Comments:', '').strip()
+        # Extracting the subjects
+        subjects = metasoup.find('div', class_='list-subjects').get_text(strip=True).replace('Subjects:', '').strip()
+
+        data = {
+            "title": title,
+            "authors": authors,
+            "comments": comments,
+            "subjects": subjects
+        }
+        metadata_list[id] = data
+        id = id + 1
+
+## array that holds all the abstracts of the articles
+id_to_texts_map = {}
+
 # Find all <dl> elements
 dl = soup.find_all('dl')
 
 # Find all <dt> elements in the <dl> element
 dt_list = dl[0].find_all('dt') 
-# dt = dt_list[0]
+
+## id of each paper
 id = 0
+## iterates through each paper
 for dt in dt_list:
     first_run = True
     # Find all <a> tags within this <dt> that have an 'href' attribute
@@ -58,22 +91,27 @@ for dt in dt_list:
         # Finds all <div> elements with the class '
         p_list = html_soup.find_all('p', class_='ltx_p')
         for p in p_list:
-            if first_run:
-                id_to_texts_map[id] = p.get_text()
+            if first_run: # If new article
+                id_to_texts_map[id] = p.get_text() # Adds a new column in dictionary
             else:
-                id_to_texts_map[id] = id_to_texts_map[id] + p.get_text()
+                id_to_texts_map[id] = id_to_texts_map[id] + p.get_text() ## If not new article, keeps adding to same element of dict
             first_run = False
+        id = id + 1
+    else:
+        ## deletes papers without html format from earlier metadata list (to match id numbering)
+        del metadata_list[id]
         id = id + 1
 
 
-# Writing to sample.json
+# Writing metadata to metadata_raw.json
+with open('metadata_raw.json', 'w') as json_file:
+    json.dump(metadata_list, json_file, indent=4)
+
+
+# Writing text data to id_to_text_raw.json
 with open("id_to_text_raw.json", "w") as outfile:
     json.dump(id_to_texts_map, outfile, indent=4)
 
-
-## how articles have paragraphs organized:
-## - ltx_p
-## - ltx_p
         
             
 
